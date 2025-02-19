786be4d6b618a65edae7bd5f62c6f4c61e3c2284
KoruptTinker <pisces1237@gmail.com>
2025-02-19 00:48:20 -0500

    Add: Task1 scripts

---
+++ homework-1/models/MLP/scripts/train_128.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#BSUB -n 1
+#BSUB -W 12:00
+#BSUB -q gpu
+#BSUB -gpu "num=1"
+#BSUB -J sec3task1n128
+#BSUB -o /share/csc591s25/bkbhayan/GenAI-for-Systems-Gym/homework-1/models/MLP/logs/out_128.%J
+#BSUB -e /share/csc591s25/bkbhayan/GenAI-for-Systems-Gym/homework-1/models/MLP/logs/err_128.%J
+
+source ~/.bashrc
+conda activate /share/$GROUP/conda_env/new_env
+cd /share/$GROUP/$USER/GenAI-for-Systems-Gym/homework-1/models/MLP
+
+python3 -m cache_replacement.policy_learning.cache_model.main \
+  --experiment_base_dir=/share/$GROUP/$USER/GenAI-for-Systems-Gym/homework-1/models/MLP/experiments \
+  --experiment_name=sec3task1n128 \
+  --cache_configs=cache_replacement/policy_learning/cache/configs/default.json \
+  --model_bindings="loss=["log_likelihood"]" \
+  --model_bindings="address_embedder.max_vocab_size=5000" \
+  --model_bindings="lstm_hidden_size=128" \
+  --train_memtrace=/share/$GROUP/traces/astar_313B_train.csv \
+  --valid_memtrace=/share/$GROUP/traces/astar_313B_valid.csv
