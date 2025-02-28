005efda64c15eb3552670313d421870c78979c15
KoruptTinker <pisces1237@gmail.com>
2025-02-28 18:13:45 -0500

    Add: RNN with attention files

---
+++ homework-1/models/RNN_with_Attention/scripts/evaluation_sl_100.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#BSUB -n 1
+#BSUB -W 48:00
+#BSUB -J evaluation_phase_rnn_with_attention_sl100
+#BSUB -o /share/csc591s25/bkbhayan/GenAI-for-Systems-Gym/homework-1/models/RNN_with_Attention/logs/out_sl_100_eval.%J
+#BSUB -e /share/csc591s25/bkbhayan/GenAI-for-Systems-Gym/homework-1/models/RNN_with_Attention/logs/err_sl_100_eval.%J
+
+source ~/.bashrc
+conda activate /share/$GROUP/conda_env/new_env
+cd /share/$GROUP/$USER/GenAI-for-Systems-Gym/homework-1/models/RNN_with_Attention
+
+python3 -m cache_replacement.policy_learning.cache.main \
+  --experiment_base_dir=/share/$GROUP/$USER/GenAI-for-Systems-Gym/homework-1/models/RNN_with_Attention/experiments\
+  --experiment_name=evaluation_phase_rnn_with_attention_sl100 \
+  --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
+  --cache_configs="cache_replacement/policy_learning/cache/configs/eviction_policy/learned.json" \
+  --memtrace_file="/share/csc591s25/traces/astar_313B_test.csv" \
+  --config_bindings="associativity=16" \
+  --config_bindings="capacity=2097152" \
+  --config_bindings="eviction_policy.scorer.checkpoint=\"/share/csc591s25/bkbhayan/GenAI-for-Systems-Gym/homework-1/models/RNN_with_Attention/experiments/rnn_with_attention_sl100/checkpoints/20000.ckpt\"" \
+  --config_bindings="eviction_policy.scorer.config_path=\"/share/csc591s25/bkbhayan/GenAI-for-Systems-Gym/homework-1/models/RNN_with_Attention/experiments/rnn_with_attention_sl100/model_config.json\"" \
+  --warmup_period=0
